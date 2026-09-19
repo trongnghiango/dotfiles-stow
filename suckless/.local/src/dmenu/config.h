a5f32e66e29dc91f910eeb8e6948777e513212be
Ka <trongnghiango@hotmail.com>
2026-09-19 11:13:59 +0700

    style(dmenu): increase font size from 14 to 16 for better readability

--- suckless/.local/src/dmenu/config.h
+++ suckless/.local/src/dmenu/config.h
@@ -4,9 +4,9 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetBrains Mono:pixelsize=14:style=SemiBold",
-	"Symbols Nerd Font:pixelsize=14:style=SemiBold",
-	"Noto Color Emoji:pixelsize=14"
+	"JetBrains Mono:pixelsize=16:style=SemiBold",
+	"Symbols Nerd Font:pixelsize=16:style=SemiBold",
+	"Noto Color Emoji:pixelsize=16"
 };
 static const unsigned int bgalpha = 0xe0;
 static const unsigned int fgalpha = OPAQUE;
